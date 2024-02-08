30607028494eed3d6ff543e30bacfe459ed858f4
AlmogDob <dobalmog123@gmail.com>
2024-02-09 01:04:11 +0200

    changing the library so it wont creat memory but only when creating a new vec | also changing the names to lower case

--- C/Vec3/test.c
+++ C/Vec3/test.c
@@ -3,9 +3,15 @@
 
 int main(void)
 {
-    Vec3 v = Vec3_new(1, 1, 0);
-    v = Vec3_rotate_around_z(v, Vec3_new(0,0,0), (3.141592*4)/2);
+    Vec3 v = vec3_new(1, 0, 0);
+    Vec3 center = vec3_new(0,0,0);
+    Vec3 u;
+    vec3_rotate_around_y(&u, &v, &center, PI/2);
 
-    VEC3_PRINT(v);
+    Vec3 res;
+
+    vec3_sub(&res, &v, &u);
+
+    VEC3_PRINT(&res);
     return 0;
 }
