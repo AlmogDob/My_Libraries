1bae37961520b180ddb5c5e95acf44ba03156b9f
Almog Dobrescu <dobalmog123@gmail.com>
2024-10-05 14:38:03 +0300

    done example for float

--- C/Dynamic_Array/test.c
+++ C/Dynamic_Array/test.c
@@ -7,22 +7,43 @@ typedef struct {
     int* elements;
 } ada_int_array;
 
+typedef struct {
+    size_t length;
+    size_t capacity;
+    float* elements;
+} ada_float_array;
+
 void print_int_ada(ada_int_array ada, char *name)
 {
     printf("%s\n", name);
     printf("capacity: %zu\n", ada.capacity);
     printf("length: %zu\n[", ada.length);
     if (ada.length == 0) {
-        printf("]\n");
+        printf("]\n\n");
         return;
     }
     for (size_t i = 0; i < ada.length - 1; i++) {
         printf("%d, ", ada.elements[i]);
     }
-    printf("%d]\n", ada.elements[ada.length - 1]);
+    printf("%d]\n\n", ada.elements[ada.length - 1]);
 }
-
 #define ADA_INT_PRINT(ada) print_int_ada(ada, #ada)
+
+void print_float_ada(ada_float_array ada, char *name)
+{
+    printf("%s\n", name);
+    printf("capacity: %zu\n", ada.capacity);
+    printf("length: %zu\n[", ada.length);
+    if (ada.length == 0) {
+        printf("]\n\n");
+        return;
+    }
+    for (size_t i = 0; i < ada.length - 1; i++) {
+        printf("%g, ", ada.elements[i]);
+    }
+    printf("%g]\n\n", ada.elements[ada.length - 1]);
+}
+#define ADA_FLOAT_PRINT(ada) print_float_ada(ada, #ada)
 
 int main()
 {
@@ -35,6 +56,16 @@ int main()
     }
     
     ADA_INT_PRINT(a);
+
+    ada_float_array b;
+
+    ada_array(float, b);
+
+    for (int i = 0; i < 69; i++) {
+        ada_appand(float, b, i/2.0);
+    }
+
+    ADA_FLOAT_PRINT(b);
 
     return 0;
 }
