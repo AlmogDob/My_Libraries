1ac874d913af6c17b0d232896b600505782e3f61
Almog Dobrescu <dobalmog123@gmail.com>
2025-01-20 17:51:47 +0200

    rearranging matrix directorys and updating the makefile

--- C/Matrix/Matrix2D/temp.c
+++ C/Matrix/Matrix2D/temp.c
@@ -1,10 +1,12 @@
 #define MATRIX_IMPLEMENTATION
 #include "Matrix.h"
+#include "time.h"
 
 int main(void)
 {
     Mat m = mat_alloc(2, 2);
-    mat_fill(m,30);
+    srand(time(0));
+    mat_rand(m, 0, 1);
 
     MAT_PRINT(m);
     return 0;
