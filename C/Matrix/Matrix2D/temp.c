65ae1d26c30631cb0aa96d9c88d5a09e8689f9a0
AlmgoDob <dobalmog123@gmail.com>
2026-01-09 22:27:53 +0200

    [MAT2D] fixed upper_triangulation and make_identity and inverse

--- C/Matrix/Matrix2D/temp.c
+++ C/Matrix/Matrix2D/temp.c
@@ -4,30 +4,21 @@
 int main(void)
 {
     int n = 3;
-    Mat2D src = mat2D_alloc(n, n);   
-    Mat2D des = mat2D_alloc(n, n);   
+    Mat2D A = mat2D_alloc(n, n);   
 
-    mat2D_rand(src, 0, 1);
-    // MAT2D_AT(src, 0, 0) = 2;
-    // MAT2D_AT(src, 1, 0) = 0;
-    // MAT2D_AT(src, 2, 0) = 1;
-    // MAT2D_AT(src, 0, 1) = 1;
-    // MAT2D_AT(src, 1, 1) = 2;
-    // MAT2D_AT(src, 2, 1) = 1;
-    // MAT2D_AT(src, 0, 2) = 3;
-    // MAT2D_AT(src, 1, 2) = 4;
-    // MAT2D_AT(src, 2, 2) = 2;
+    MAT2D_AT(A, 0, 0) = 2;
+    MAT2D_AT(A, 1, 0) = 0;
+    MAT2D_AT(A, 2, 0) = 0;
+    MAT2D_AT(A, 0, 1) = 0;
+    MAT2D_AT(A, 1, 1) = 3;
+    MAT2D_AT(A, 2, 1) = 4;
+    MAT2D_AT(A, 0, 2) = 0;
+    MAT2D_AT(A, 1, 2) = 4;
+    MAT2D_AT(A, 2, 2) = 9;
 
-    MAT2D_PRINT(src); 
+    MAT2D_PRINT(A); 
 
-    mat2D_invert(des, src);
-
-    MAT2D_PRINT(des); 
-
-
-
-    mat2D_free(src);
-    mat2D_free(des);
+    mat2D_free(A);
 
     return 0;
 }
