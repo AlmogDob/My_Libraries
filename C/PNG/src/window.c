f7c45b9fb6c3b9f1fdfc08b3c1202f30f1c78f45
Almog Dobrescu <almogd@campus.technion.ac.il>
2026-03-11 22:17:45 +0200

    [apng] first commit

---
+++ C/PNG/src/window.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+#define APL_SETUP
+#define APL_INPUT
+#define APL_UPDATE
+#define APL_RENDER
+#define MATRIX2D_IMPLEMENTATION
+#define ALMOG_PLATFORM_LIBRARY_IMPLEMENTATION
+#include "Almog_Platform_Library.h"
+#define ALMOG_DRAW_LIBRARY_IMPLEMENTATION
+#include "Almog_Draw_Library.h"
+
+enum Apl_Return_Types apl_setup(struct Apl_Window_State *ws)
+{
+    ws->wanted_fps = 40;
+    ws->to_limit_fps = false;
+    APL_UNUSED(ws);
+
+    return APL_SUCCESS;
+}
+
+enum Apl_Return_Types apl_update(struct Apl_Window_State *ws)
+{
+    APL_UNUSED(ws);
+
+    return APL_SUCCESS;
+}
+
+enum Apl_Return_Types apl_render(struct Apl_Window_State *ws)
+{
+    adl_circle_fill(ws->window_pixels_mat, 100, 100, 100, APL_COLOR_WHITE_hexARGB, ADL_DEFAULT_OFFSET_ZOOM);
+    APL_UNUSED(ws);
+
+    return APL_SUCCESS;
+}
+
+enum Apl_Return_Types apl_input(struct Apl_Window_State *ws)
+{
+    if (ws->buttons.w_is_pressed) {
+        printf("w");
+    }
+    if (ws->buttons.a_is_pressed) {
+        printf("a");
+    }
+    if (ws->buttons.s_is_pressed) {
+        printf("s");
+    }
+    if (ws->buttons.d_is_pressed) {
+        printf("d");
+    }
+    if (ws->buttons.space_bar_is_pressed) {
+        printf("\n");
+    }
+    APL_UNUSED(ws);
+
+    return APL_SUCCESS;
+}
