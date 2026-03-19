0c6d033589b074b14124aab9c65ed68d547f49ac
Almog Dobrescu <almogd@campus.technion.ac.il>
2026-03-19 20:57:54 +0200

    [ah] generating codes table

--- C/Huffman/src/include/Almog_Huffman.h
+++ C/Huffman/src/include/Almog_Huffman.h
@@ -31,6 +31,21 @@ struct Ah_Tree {
     struct Ah_Tree_Node *elements;
 };
 
+#ifndef AH_ASSERT
+#include <assert.h>
+#define AH_ASSERT assert
+#endif
+
+#ifndef AH_CALLOC
+#include <stdlib.h>
+#define AH_CALLOC calloc
+#endif
+
+#ifndef AH_FREE
+#include <stdlib.h>
+#define AH_FREE free
+#endif
+
 #define AH_DEF static inline
 #define AH_UNUSED(x) ((void)x)
 #define ah_dprintSTRING(expr) printf("[Info] %s:%d:\n" #expr " = %s\n", __FILE__, __LINE__, expr)
@@ -46,11 +61,22 @@ struct Ah_Tree {
 #define ah_dprintERROR(fmt, ...) \
     fprintf(stderr, "[Error] %s:%d:\n%*sIn function '%s':\n%*s" fmt "\n", __FILE__, __LINE__, 8, "", __func__, 8, "", __VA_ARGS__)
 
-AH_DEF void     ah_qsort(struct Ah_Table_Element v[], int left, int right);
-AH_DEF void     ah_swap(struct Ah_Table_Element v[], int i, int j);
-AH_DEF void     ah_table_element_print(struct Ah_Table_Element e);
-AH_DEF int      ah_table_find_name(struct Ah_Table table, uint8_t name);
-AH_DEF void     ah_table_print(struct Ah_Table table);
+AH_DEF void                 ah_qsort(struct Ah_Table_Element v[], int left, int right);
+AH_DEF void                 ah_swap(struct Ah_Table_Element v[], int i, int j);
+AH_DEF void                 ah_table_element_print(struct Ah_Table_Element e);
+AH_DEF int                  ah_table_find_name(struct Ah_Table table, uint8_t name);
+AH_DEF void                 ah_table_print(struct Ah_Table table);
+AH_DEF int                  ah_tree_find_node(struct Ah_Tree tree, struct Ah_Tree_Node node);
+AH_DEF struct Ah_Tree_Node  ah_tree_node_create_from_table_element(struct Ah_Table_Element e);
+AH_DEF struct Ah_Tree_Node  ah_tree_nodes_join(struct Ah_Tree tree, size_t a_index, size_t b_index);
+AH_DEF void                 ah_tree_node_print(struct Ah_Tree_Node t);
+AH_DEF struct Ah_Tree_Node  ah_tree_pop(struct Ah_Tree *tree);
+AH_DEF void                 ah_tree_print(struct Ah_Tree t);
+AH_DEF void                 ah_tree_print_ascii(const struct Ah_Tree *tree);
+AH_DEF void                 ah_tree_print_ascii_from(const struct Ah_Tree *tree, int root_index);
+AH_DEF void                 ah_tree_print_ascii_node_label(struct Ah_Tree_Node node);
+AH_DEF void                 ah_tree_print_ascii_rec(const struct Ah_Tree *tree, int node_index, bool *has_more, int depth, bool is_left, bool is_root);
+AH_DEF void                 ah_tree_push(struct Ah_Tree *tree, struct Ah_Tree_Node node);
 
 #endif /*ALMOG_HUFFMAN_H_*/
 
@@ -105,6 +131,183 @@ AH_DEF void ah_table_print(struct Ah_Table table)
     for (size_t i = 0; i < table.length; i++) {
         printf("%3zu: ", i);
         ah_table_element_print(table.elements[i]);
+    }
+}
+
+AH_DEF int ah_tree_find_node(struct Ah_Tree tree, struct Ah_Tree_Node node)
+{
+    for (size_t i = 0; i < tree.length; i++) {
+        struct Ah_Tree_Node current_node = tree.elements[i];
+        if (current_node.count == node.count &&
+            current_node.left_index == node.left_index &&
+            current_node.name == node.name &&
+            current_node.right_index == node.right_index) {
+                return (int)i;
+            }
+    }
+
+    return -1;
+}
+
+AH_DEF struct Ah_Tree_Node ah_tree_node_create_from_table_element(struct Ah_Table_Element e)
+{
+    return (struct Ah_Tree_Node){
+        .count = e.count,
+        .name = e.name,
+        .left_index = -1,
+        .right_index = -1
+    };
+}
+
+AH_DEF struct Ah_Tree_Node ah_tree_nodes_join(struct Ah_Tree tree, size_t a_index, size_t b_index)
+{
+    AH_ASSERT(a_index >= 0 && (size_t)a_index < tree.length);
+    AH_ASSERT(b_index >= 0 && (size_t)b_index < tree.length);
+
+    return (struct Ah_Tree_Node){
+        .count = tree.elements[a_index].count + tree.elements[b_index].count,
+        .name = -1,
+        .left_index = (int)a_index,
+        .right_index = (int)b_index,
+    };
+}
+
+AH_DEF void ah_tree_node_print(struct Ah_Tree_Node t)
+{
+    if (isprint(t.name) && t.name != '\\' && t.name != '\'') {
+        printf("name: '%c' | count: %3zu | (%3d,%3d)\n", t.name, t.count, t.left_index, t.right_index);
+    } else {
+        printf("name: %3d | count: %3zu | (%3d,%3d)\n", t.name, t.count, t.left_index, t.right_index);
+    }
+}
+
+AH_DEF struct Ah_Tree_Node ah_tree_pop(struct Ah_Tree *tree)
+{
+    AH_ASSERT(tree != NULL);
+    AH_ASSERT(tree->length > 0 && "ah_tree_pop on empty tree");
+    return tree->elements[--tree->length];
+}
+
+AH_DEF void ah_tree_print(struct Ah_Tree t)
+{
+    for (size_t i = 0; i < t.length; i++) {
+        printf("%3zu: ", i);
+        ah_tree_node_print(t.elements[i]);
+    }
+}
+
+AH_DEF void ah_tree_print_ascii(const struct Ah_Tree *tree)
+{
+    if (tree == NULL || tree->length == 0) {
+        printf("(empty tree)\n");
+        return;
+    }
+
+    ah_tree_print_ascii_from(tree, (int)tree->length - 1);
+}
+
+AH_DEF void ah_tree_print_ascii_from(const struct Ah_Tree *tree, int root_index)
+{
+    if (tree == NULL || tree->length == 0) {
+        printf("(empty tree)\n");
+        return;
+    }
+
+    if (root_index < 0 || (size_t)root_index >= tree->length) {
+        printf("(invalid root index: %d)\n", root_index);
+        return;
+    }
+
+    bool *has_more = AH_CALLOC(tree->length + 1, sizeof(bool));
+    if (has_more == NULL) {
+        printf("(memory allocation failed)\n");
+        return;
+    }
+
+    ah_tree_print_ascii_rec(tree, root_index, has_more, 0, false, true);
+
+    AH_FREE(has_more);
+}
+
+AH_DEF void ah_tree_print_ascii_node_label(struct Ah_Tree_Node node)
+{
+    if (node.name == -1) {
+        printf("[* count=%zu]\n", node.count);
+        return;
+    }
+
+    unsigned char ch = (unsigned char)node.name;
+
+    if (isprint(ch) && ch != '\\' && ch != '\'') {
+        printf("['%c' count=%zu]\n", ch, node.count);
+    } else {
+        printf("[0x%02X count=%zu]\n", ch, node.count);
+    }
+}
+
+AH_DEF void ah_tree_print_ascii_rec(const struct Ah_Tree *tree, int node_index, bool *has_more, int depth, bool is_left, bool is_root)
+{
+    if (node_index < 0) {
+        return;
+    }
+
+    struct Ah_Tree_Node node = tree->elements[node_index];
+
+    for (int i = 0; i < depth; i++) {
+        printf("%s", has_more[i] ? "|   " : "    ");
+    }
+
+    if (!is_root) {
+        printf("%s", is_left ? "\b\b\b\b|-- " : "\b\b\b\b\\-- ");
+    }
+
+    ah_tree_print_ascii_node_label(node);
+
+    bool has_left = node.left_index != -1;
+    bool has_right = node.right_index != -1;
+
+    if (has_left) {
+        has_more[depth] = has_right;
+        ah_tree_print_ascii_rec(
+            tree,
+            node.left_index,
+            has_more,
+            depth + 1,
+            true,
+            false
+        );
+    }
+
+    if (has_right) {
+        has_more[depth] = false;
+        ah_tree_print_ascii_rec(
+            tree,
+            node.right_index,
+            has_more,
+            depth + 1,
+            false,
+            false
+        );
+    }
+}
+
+AH_DEF void ah_tree_push(struct Ah_Tree *tree, struct Ah_Tree_Node node)
+{
+    /* If too slow, might want to implement min-heap algorithm */
+    size_t i = 0;
+
+    while (i < tree->length && tree->elements[i].count > node.count) {
+        i++;
+    }
+
+    while (i < tree->length && tree->elements[i].count == node.count) {
+        i++;
+    }
+
+    if (i == tree->length) {
+        ada_appand(struct Ah_Tree_Node, *tree, node);
+    } else {
+        ada_insert(struct Ah_Tree_Node, *tree, node, (int)i);
     }
 }
 
--- C/Huffman/src/temp.c
+++ C/Huffman/src/temp.c
@@ -1,183 +1,101 @@
 #define ALMOG_HUFFMAN_IMPLEMENTATION
 #include "./include/Almog_Huffman.h"
 
-struct Ah_Tree_Node ah_tree_node_create_from_table_element(struct Ah_Table_Element e)
+struct Ah_Code {
+    uint8_t name;
+    uint8_t curser;
+    char code[256];
+};
+
+struct Ah_Code_Table {
+    size_t length;
+    size_t capacity;
+    struct Ah_Code *elements;
+};
+
+AH_DEF void ah_code_print(struct Ah_Code code) 
 {
-    return (struct Ah_Tree_Node){
-        .count = e.count,
-        .name = e.name,
-        .left_index = -1,
-        .right_index = -1
-    };
+    if (isprint(code.name) && code.name != '\\' && code.name != '\'') {
+        printf("name: '%c' -> code: %s\n", code.name, code.code);
+    } else {
+        printf("name: 0x%02x -> code: %s\n", code.name, code.code);
+    }
 }
 
-struct Ah_Tree_Node ah_tree_nodes_join(struct Ah_Tree tree, size_t a_index, size_t b_index)
-{
-    return (struct Ah_Tree_Node){
-        .count = tree.elements[a_index].count + tree.elements[b_index].count,
-        .name = -1,
-        .left_index = (int)a_index,
-        .right_index = (int)b_index,
-    };
-}
-
-void ah_tree_node_print(struct Ah_Tree_Node t)
-{
-    printf("name: %4d | count: %3zu | (%3d,%3d)\n", t.name, t.count, t.left_index, t.right_index);
-}
-
-void ah_tree_print(struct Ah_Tree t)
+AH_DEF void ah_code_table_print(struct Ah_Code_Table t)
 {
     for (size_t i = 0; i < t.length; i++) {
-        printf("%3zu: ", i);
-        ah_tree_node_print(t.elements[i]);
+        printf("%zu: ", i);
+        ah_code_print(t.elements[i]);
     }
 }
 
-struct Ah_Tree_Node ah_tree_pop(struct Ah_Tree *tree)
+AH_DEF bool ah_code_create_from_tree_leaf(struct Ah_Tree tree, int current_node_index, size_t leaf_index, struct Ah_Code *code)
 {
-    return tree->elements[--tree->length];
-}
+    AH_ASSERT(tree.elements[leaf_index].left_index == -1 && "not a leaf");
+    AH_ASSERT(tree.elements[leaf_index].right_index == -1 && "not a leaf");
 
-void ah_tree_push(struct Ah_Tree *tree, struct Ah_Tree_Node node)
-{
-    size_t i = 0;
+    if (current_node_index < 0 || (size_t)current_node_index >= tree.length) {
+        return false;
+    }
+    
+    printf("%3d: ", current_node_index);
+    ah_tree_node_print(tree.elements[current_node_index]);
 
-    while (i < tree->length && tree->elements[i].count > node.count) {
-        i++;
+    if (current_node_index == leaf_index) {
+        code->name = (uint8_t)tree.elements[leaf_index].name;
+        code->code[code->curser] = '\0';
+        return true;
+    } else if (tree.elements[current_node_index].left_index == -1 && tree.elements[current_node_index].right_index == -1) {
+        code->code[code->curser] = '\0';
+        ah_dprintSTRING(code->code);
+        return false;
     }
 
-    while (i < tree->length && tree->elements[i].count == node.count) {
-        i++;
-    }
-
-    if (i == tree->length) {
-        ada_appand(struct Ah_Tree_Node, *tree, node);
+    code->code[code->curser++] = '0'; 
+    if (!ah_code_create_from_tree_leaf(tree, tree.elements[current_node_index].left_index, leaf_index, code)) {
+        code->curser--;
+        code->code[code->curser++] = '1';
     } else {
-        ada_insert(struct Ah_Tree_Node, *tree, node, (int)i);
-    }
-}
-
-int ah_tree_find_node(struct Ah_Tree tree, struct Ah_Tree_Node node)
-{
-    for (size_t i = 0; i < tree.length; i++) {
-        struct Ah_Tree_Node current_node = tree.elements[i];
-        if (current_node.count == node.count &&
-            current_node.left_index == node.left_index &&
-            current_node.name == node.name &&
-            current_node.right_index == node.right_index) {
-                return (int)i;
-            }
+        return true;
     }
 
-    return -1;
-}
-
-static void ah_tree_print_ascii_node_label(struct Ah_Tree_Node node)
-{
-    if (node.name == -1) {
-        printf("[* count=%zu]\n", node.count);
-        return;
-    }
-
-    unsigned char ch = (unsigned char)node.name;
-
-    if (isprint(ch) && ch != '\\' && ch != '\'') {
-        printf("['%c' count=%zu]\n", ch, node.count);
+    if (!ah_code_create_from_tree_leaf(tree, tree.elements[current_node_index].right_index, leaf_index, code)) {
+        code->curser--;
+        return false;
     } else {
-        printf("[0x%02X count=%zu]\n", ch, node.count);
+        return true;
     }
 }
 
-static void ah_tree_print_ascii_rec(
-    const struct Ah_Tree *tree,
-    int node_index,
-    bool *has_more,
-    int depth,
-    bool is_left,
-    bool is_root
-)
+AH_DEF void ah_code_table_create(struct Ah_Tree tree, struct Ah_Code_Table *table, struct Ah_Code *buff, int current_node_index)
 {
-    if (node_index < 0) {
+    AH_ASSERT(current_node_index >= 0 || (size_t)current_node_index < tree.length);
+    // printf("%3zu: ", current_node_index);
+    // ah_tree_node_print(tree.elements[current_node_index]);
+
+    if (tree.elements[current_node_index].left_index == -1 && tree.elements[current_node_index].right_index == -1) {
+        buff->name = (uint8_t)tree.elements[current_node_index].name;
+        if (buff->curser == 0) {
+            buff->code[buff->curser++] = '0';
+        }
+        buff->code[buff->curser] = '\0';
+        // ah_dprintSTRING(buff->code);
+        ada_appand(struct Ah_Code, *table, *buff);
+        // ah_code_table_print(*table);
         return;
     }
 
-    struct Ah_Tree_Node node = tree->elements[node_index];
-
-    for (int i = 0; i < depth; i++) {
-        printf("%s", has_more[i] ? "|   " : "    ");
-    }
-
-    if (!is_root) {
-        printf("%s", is_left ? "\b\b\b\b|-- " : "\b\b\b\b\\-- ");
-    }
-
-    ah_tree_print_ascii_node_label(node);
-
-    bool has_left = node.left_index != -1;
-    bool has_right = node.right_index != -1;
-
-    if (has_left) {
-        has_more[depth] = has_right;
-        ah_tree_print_ascii_rec(
-            tree,
-            node.left_index,
-            has_more,
-            depth + 1,
-            true,
-            false
-        );
-    }
-
-    if (has_right) {
-        has_more[depth] = false;
-        ah_tree_print_ascii_rec(
-            tree,
-            node.right_index,
-            has_more,
-            depth + 1,
-            false,
-            false
-        );
-    }
-}
-
-void ah_tree_print_ascii_from(const struct Ah_Tree *tree, int root_index)
-{
-    if (tree == NULL || tree->length == 0) {
-        printf("(empty tree)\n");
-        return;
-    }
-
-    if (root_index < 0 || (size_t)root_index >= tree->length) {
-        printf("(invalid root index: %d)\n", root_index);
-        return;
-    }
-
-    bool *has_more = calloc(tree->length + 1, sizeof(bool));
-    if (has_more == NULL) {
-        printf("(memory allocation failed)\n");
-        return;
-    }
-
-    ah_tree_print_ascii_rec(tree, root_index, has_more, 0, false, true);
-
-    free(has_more);
-}
-
-void ah_tree_print_ascii(const struct Ah_Tree *tree)
-{
-    if (tree == NULL || tree->length == 0) {
-        printf("(empty tree)\n");
-        return;
-    }
-
-    ah_tree_print_ascii_from(tree, (int)tree->length - 1);
+    buff->code[buff->curser++] = '0'; 
+    ah_code_table_create(tree, table, buff, tree.elements[current_node_index].left_index);
+    buff->curser--;
+    buff->code[buff->curser++] = '1';
+    ah_code_table_create(tree, table, buff, tree.elements[current_node_index].right_index);
+    buff->curser--;
 }
 
 int main(void)
 {
-    
     FILE *fp = fopen("../input.txt", "rt");
     // FILE *fp = fopen("../src/temp.c", "rt");
     if (fp == NULL) {
@@ -201,12 +119,14 @@ int main(void)
         }
     }
     printf("\n");
+    if (table.length == 0) {
+        ah_dprintERROR("%s", "Empty input file.");
+        return 0;
+    }
 
     ah_qsort(table.elements, 0, (int)table.length-1);
 
     ah_table_print(table);
-
-    printf("---------------------------------\n");
 
     struct Ah_Tree tree = {0};
     struct Ah_Tree priority_queue = {0};
@@ -218,8 +138,6 @@ int main(void)
         ada_appand(struct Ah_Tree_Node, priority_queue, ah_tree_node_create_from_table_element(table.elements[i]));
     }
 
-    ah_tree_print(priority_queue);
-    
     for ( ; priority_queue.length > 1 ;) {
         struct Ah_Tree_Node a = ah_tree_pop(&priority_queue);
         struct Ah_Tree_Node b = ah_tree_pop(&priority_queue);
@@ -230,7 +148,15 @@ int main(void)
 
     printf("##############################\n");
 
-    ah_tree_print_ascii(&tree);
+    // ah_tree_print(tree);
+    // ah_tree_print_ascii(&tree);
+
+    struct Ah_Code buff = {0};
+    struct Ah_Code_Table code_table = {0};
+    ada_init_array(struct Ah_Code, code_table);
+    ah_code_table_create(tree, &code_table, &buff, (int)tree.length-1);
+
+    ah_code_table_print(code_table); 
 
     return 0;
 }
