6a1fbd67be930d7b8d5beeb0a5a316f651263c62
Almog Dobrescu <almogd@campus.technion.ac.il>
2026-06-26 01:41:30 +0300

    [amd] I think I implemented it correctly

--- C/Memory_Debug/Almog_Memory_Debug.h
+++ C/Memory_Debug/Almog_Memory_Debug.h
@@ -2,8 +2,8 @@
 #define ALMOG_MEMORY_DEBUG_H_
 
 #include <stdio.h>
-
-#include "Almog_String_Manipulation.h"
+#include <stdint.h>
+#include <stdbool.h>
 
 #ifndef AMD_MALLOC
 #include <stdlib.h>
@@ -17,6 +17,10 @@
 #include <stdlib.h>
 #define AMD_REALLOC realloc
 #endif /*AMD_REALLOC*/
+#ifndef AMD_FREE
+#include <stdlib.h>
+#define AMD_FREE free
+#endif /*AMD_FREE*/
 
 /**
  * @name Debug-print helpers
@@ -183,20 +187,24 @@ struct Amd_Mem_Alloc_Buf {
     void *buf;
 };
 
+#define AMD_MAX_LEN (int)1e3
 struct Amd_Mem_Alloc_Line {
     size_t capacity;
     size_t length;
-    struct Amd_Mem_Alloc_Buf elements;
+    struct Amd_Mem_Alloc_Buf *elements;
     size_t line;
-    char file[ASM_MAX_LEN];
+    char file[AMD_MAX_LEN];
     size_t size;
-    size_t allocated_over_all;
+    size_t allocations_over_all;
+    size_t bytes_allocated_over_all;
     size_t freed;
 };
 
-#define AMD_MEM_ALLOC_LINES_NUM 1024
+#define AMD_MEM_ALLOC_LINES_MAX_NUM 1024
 #define AMD_MEM_OVER_ALLOC 32
 #define AMD_MEM_MAGIC_NUMBER 132
+
+#define amd_min(a, b) ((a) < (b) ? (a) : (b))
 
 #ifndef AMD_SUCCESS 
     #define AMD_SUCCESS 1
@@ -213,16 +221,265 @@ struct Amd_Mem_Alloc_Line {
     #endif
 #endif
 
-
-
+AMD_DEF bool    amd_debug_mem(void);
+AMD_DEF void    amd_debug_mem_add(void *p, size_t size, char *file, size_t line);
+AMD_DEF size_t  amd_debug_mem_consumption(void);
+AMD_DEF void    amd_debug_mem_free(void *buf);
+AMD_DEF void *  amd_debug_mem_malloc(size_t size, char *file, size_t line);
+AMD_DEF void    amd_debug_mem_print(size_t min_allocs);
+AMD_DEF void *  amd_debug_mem_realloc(void *p, size_t size, char *file, size_t line);
+AMD_DEF bool    amd_debug_mem_remove(void *buf);
+AMD_DEF void    amd_debug_mem_reset(void);
+AMD_DEF int     amd_strncmp(const char *s1, const char *s2, const size_t N);
+AMD_DEF int     amd_strncpy(char * const s1, const char * const s2, const size_t N);
 
 #endif /*ALMOG_MEMORY_DEBUG_H_*/
 
 #ifdef ALMOG_MEMORY_DEBUG_IMPLEMENTATION
 #undef ALMOG_MEMORY_DEBUG_IMPLEMENTATION
 
-struct Amd_Mem_Alloc_Line mem_alloc_lines[AMD_MEM_ALLOC_LINES_NUM];
+struct Amd_Mem_Alloc_Line mem_alloc_lines[AMD_MEM_ALLOC_LINES_MAX_NUM];
 size_t mem_alloc_lines_count = 0;
 
+AMD_DEF bool amd_debug_mem(void)
+{
+    bool output = AMD_SUCCESS;
+    size_t i, j, k;
+    for (i = 0; i < mem_alloc_lines_count; i++) {
+        for (j = 0; j < mem_alloc_lines[i].length; j++) {
+            uint8_t *buf = mem_alloc_lines[i].elements[j].buf;
+            size_t size = mem_alloc_lines[i].elements[j].size;
+            for (k = 0; k < AMD_MEM_OVER_ALLOC; k++) {
+                if (buf[size + k] != AMD_MEM_MAGIC_NUMBER) {
+                    break;
+                }
+            }
+            if (k < AMD_MEM_OVER_ALLOC) {
+                amd_dprintERROR("MEM ERROR: overshoot at line %zu in file '%s'.", mem_alloc_lines[i].line, mem_alloc_lines[i].file);
+                output = AMD_FAIL;
+            }
+        }
+    }
+
+    return output;
+}
+
+AMD_DEF void amd_debug_mem_add(void *p, size_t size, char *file, size_t line)
+{
+    size_t i;
+    for (i = 0; i < AMD_MEM_OVER_ALLOC; i++) {
+        ((uint8_t *)p)[size + i] = AMD_MEM_MAGIC_NUMBER;
+    }
+    for (i = 0; i < mem_alloc_lines_count; i++) {
+        if (line == mem_alloc_lines[i].line) {
+            if (amd_strncmp(file, mem_alloc_lines[i].file, AMD_MAX_LEN)) {
+                break;
+            }
+        }
+    }
+    if (i < mem_alloc_lines_count) {
+        struct Amd_Mem_Alloc_Buf mem_alloc_buf = {
+            .buf = p,
+            .size = size
+        };
+        amd_ada_appand(struct Amd_Mem_Alloc_Buf, mem_alloc_lines[i], mem_alloc_buf);
+        mem_alloc_lines[i].size += size;
+        mem_alloc_lines[i].allocations_over_all++;
+        mem_alloc_lines[i].bytes_allocated_over_all += size;
+    } else {
+        if (i < AMD_MEM_ALLOC_LINES_MAX_NUM) {
+            struct Amd_Mem_Alloc_Buf mem_alloc_buf = {
+                .buf = p,
+                .size = size
+            };
+            amd_ada_init_array(struct Amd_Mem_Alloc_Buf, mem_alloc_lines[i]);
+            mem_alloc_lines[i].line = line;
+            amd_strncpy(mem_alloc_lines[i].file, file, AMD_MAX_LEN);
+            mem_alloc_lines[i].size = 0;
+            mem_alloc_lines[i].allocations_over_all = 0;
+            mem_alloc_lines[i].freed = 0;
+
+            amd_ada_appand(struct Amd_Mem_Alloc_Buf, mem_alloc_lines[i], mem_alloc_buf);
+            mem_alloc_lines[i].size += size;
+            mem_alloc_lines[i].bytes_allocated_over_all += size;
+            mem_alloc_lines[i].allocations_over_all++;
+            mem_alloc_lines_count++;
+        }
+    }
+}
+
+AMD_DEF size_t amd_debug_mem_consumption(void)
+{
+    size_t sum = 0;
+    for (size_t i = 0; i < mem_alloc_lines_count; i++) {
+        sum += mem_alloc_lines[i].size;
+    }
+
+    return sum;
+}
+
+AMD_DEF void amd_debug_mem_free(void *buf)
+{
+    if (buf == NULL) {
+        return;
+    }
+    if (AMD_FAIL == amd_debug_mem_remove(buf)) {
+        amd_dprintERROR("MEM ERROR: error occurred while trying to free buf: %p.", buf);
+        AMD_EXIT(1);
+    }
+    AMD_FREE(buf);
+}
+
+AMD_DEF void *amd_debug_mem_malloc(size_t size, char *file, size_t line)
+{
+    void *p = AMD_MALLOC(size + AMD_MEM_OVER_ALLOC);
+    if (NULL == p) {
+        amd_dprintERROR("MEM ERROR: malloc returns NULL when trying to allocate %zu bytes at line %zu of file '%s'.", size, line, file);
+        /* maybe print more debug info */
+        AMD_EXIT(1);
+    }
+    for (size_t i = 0; i < size + AMD_MEM_OVER_ALLOC; i++) {
+        ((uint8_t *)p)[i] = AMD_MEM_MAGIC_NUMBER + 1;
+    }
+    amd_debug_mem_add(p, size, file, line);
+
+    return p;
+}
+
+AMD_DEF void amd_debug_mem_print(size_t min_allocs)
+{
+    amd_dprintINFO("%s", "Memory report:");
+    printf("\33[A");
+    for (size_t i = 0; i < mem_alloc_lines_count; i++) {
+        if (min_allocs < mem_alloc_lines[i].allocations_over_all) {
+            printf("%*.s%s:%zu\n", 7, "", mem_alloc_lines[i].file, mem_alloc_lines[i].line);
+            printf("%*.s - Allocations: %zu\n", 7, "", mem_alloc_lines[i].allocations_over_all);
+            printf("%*.s - Frees: %zu\n", 7, "", mem_alloc_lines[i].freed);
+            printf("%*.s - Bytes allocated over all: %zu\n", 7, "", mem_alloc_lines[i].bytes_allocated_over_all);
+            printf("%*.s - Bytes remained not freed: %zu\n", 7, "", mem_alloc_lines[i].size);
+        }
+    }
+}
+
+AMD_DEF void *amd_debug_mem_realloc(void *p, size_t size, char *file, size_t line)
+{
+    size_t i, j = 0, k, move;
+    void *p2;
+    if (p == NULL) {
+        return amd_debug_mem_malloc(size, file, line);
+    }
+    for (i = 0; i < mem_alloc_lines_count; i++) {
+        for (j = 0; j < mem_alloc_lines[i].length; j++) {
+            if (mem_alloc_lines[i].elements[j].buf == p) {
+                break;
+            }
+        }
+        if (j < mem_alloc_lines[i].length) {
+            break;
+        }
+    }
+    if (i == mem_alloc_lines_count) {
+        amd_dprintERROR("MEM DEBUGGER ERROR: tring to reallocate an unknown pointer %p in %s:%zu.", p, file, line);
+        for (i = 0; i < mem_alloc_lines_count; i++) {
+            for (j = 0; j < mem_alloc_lines[i].length; j++) {
+                uint8_t *buf = mem_alloc_lines[i].elements[j].buf;
+                for (k = 0; k < mem_alloc_lines[i].elements[j].size; k++) {
+                    if (&buf[k] == (uint8_t *)p) {
+                        amd_dprintERROR("Trying to reallocate pointer at %zu bytes (out of %zu) in to allocation made in %s:%zu.", k, mem_alloc_lines[i].elements[j].size, mem_alloc_lines[i].file, mem_alloc_lines[i].line);
+                    }
+                }
+            }
+        }
+        AMD_EXIT(1);
+    }
+    move = mem_alloc_lines[i].elements[j].size;
+    if (move > size) move = size;
+
+    p2 = AMD_MALLOC(size + AMD_MEM_OVER_ALLOC);
+    if (NULL == p2) {
+        amd_dprintERROR("MEM ERROR: malloc returns NULL when trying to allocate %zu bytes at line %zu of file '%s'.", size, line, file);
+        /* maybe print more debug info */
+        AMD_EXIT(1);
+    }
+    for (i = 0; i < size + AMD_MEM_OVER_ALLOC; i++) {
+        ((uint8_t *)p2)[i] = AMD_MEM_MAGIC_NUMBER + 1;
+    }
+    for (i = 0; i < move; i++) {
+        ((uint8_t *)p2)[i] = ((uint8_t *)p)[i];
+    }
+    amd_debug_mem_add(p2, size, file, line);
+    amd_debug_mem_remove(p);
+    AMD_FREE(p);
+
+    return p2;
+}
+
+AMD_DEF bool amd_debug_mem_remove(void *buf)
+{
+    size_t i, j, k;
+    for (i = 0; i < mem_alloc_lines_count; i++) {
+        for (j = 0; j < mem_alloc_lines[i].length; j++) {
+            if (mem_alloc_lines[i].elements[j].buf == buf) {
+                for (k = 0; k < AMD_MEM_OVER_ALLOC; k++) {
+                    if (((uint8_t *)buf)[mem_alloc_lines[i].elements[j].size + k] != AMD_MEM_MAGIC_NUMBER) {
+                        break;
+                    }
+                }
+                if (k < AMD_MEM_OVER_ALLOC) {
+                    amd_dprintERROR("MEM ERROR: overshoot at line %zu in file '%s'.", mem_alloc_lines[i].line, mem_alloc_lines[i].file);
+                }
+                mem_alloc_lines[i].size -= mem_alloc_lines[i].elements[j].size;
+                amd_ada_remove_unordered(struct Amd_Mem_Alloc_Buf, mem_alloc_lines[i], j);
+                mem_alloc_lines[i].freed++;
+                return AMD_SUCCESS;
+            }
+        }
+    }
+    return AMD_FAIL;
+}
+
+AMD_DEF void amd_debug_mem_reset(void)
+{
+    for (size_t i = 0; i < mem_alloc_lines_count; i++) {
+        AMD_FREE(mem_alloc_lines[i].elements);
+    }
+    mem_alloc_lines_count = 0;
+}
+
+AMD_DEF int amd_strncmp(const char *s1, const char *s2, const size_t N)
+{
+    size_t i = 0;
+    while (i < N) {
+        if (s1[i] == '\0' && s2[i] == '\0') {
+            break;
+        }
+        if (s1[i] != s2[i] || (s1[i] == '\0') || (s2[i] == '\0')) {
+            return 0;
+        }
+        i++;
+    }
+    return 1;
+}
+
+AMD_DEF int amd_strncpy(char * const s1, const char * const s2, const size_t N)
+{
+    if (N == 0) return 0;
+
+    size_t n = amd_min(N, (size_t)AMD_MAX_LEN - 1);
+
+    size_t i;
+    for (i = 0; i < n && s2[i] != '\0'; i++) {
+        s1[i] = s2[i];
+    }
+    s1[i] = '\0';
+
+    return (int)i;
+}
 
 #endif /*ALMOG_MEMORY_DEBUG_IMPLEMENTATION*/
+
+#ifdef AMD_MEMORY_DEBUG
+    #define malloc(n) amd_debug_mem_malloc(n, __FILE__, __LINE__)
+    #define realloc(n, m) amd_debug_mem_realloc(n, m, __FILE__, __LINE__)
+    #define free(n) amd_debug_mem_free(n)
+#endif
--- C/Memory_Debug/temp.c
+++ C/Memory_Debug/temp.c
@@ -1,13 +1,37 @@
-#include <stdio.h>
-
-#define ALMOG_STRING_MANIPULATION_IMPLEMENTATION
+#define AMD_MEMORY_DEBUG
 #define ALMOG_MEMORY_DEBUG_IMPLEMENTATION
 #include "./Almog_Memory_Debug.h"
 
+struct int_array {
+    size_t capacity;
+    size_t length;
+    int *elements; 
+};
+
+void int_array_print(struct int_array list)
+{
+    for (size_t i = 0; i < list.length; i++) {
+        printf("%d, ", list.elements[i]);
+    }
+    printf("\n");
+}
 
 int main(void)
 {
     printf("Hello, World!\n");
+
+    struct int_array list= {0};
+    amd_ada_init_array(int, list);
+    for (size_t i = 0; i < 15000; i++) {
+        amd_ada_appand(int, list, (int)i);
+    }
+
+    // int_array_print(list);
+
+    // free(list.elements);
+
+    amd_debug_mem_print(0);
+    amd_dprintSIZE_T(list.capacity);
 
     return 0;
 }
